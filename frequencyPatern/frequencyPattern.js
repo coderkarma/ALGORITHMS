8fdfc31151a1448262730fef4165359a32615963
coderkarma <karmadrukpa11@gmail.com>
2020-02-20 16:22:11 -0800

    frequency pattern

---
+++ frequencyPatern/frequencyPattern.js
@@ -0,0 +1,55 @@
+// Big 0 complexity,
+//quadratic in nested loops since indexOf is potential checking every element
+
+let arr1 = [1, 2, 3, 10];
+let arr2 = [4, 1, 9, 100];
+
+function same(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    let correctIndex = arr2.indexOf(arr1[i] ** 2);
+    console.log(correctIndex);
+
+    if (correctIndex === -1) {
+      return false;
+    }
+    console.log(arr2);
+    arr2.splice(correctIndex, 1);
+  }
+  return true;
+}
+
+// Two sparate loop is better than 2 nested loop
+// Big O, O(n) ---- Here :)
+function same(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  let frequencyCounter1 = {};
+  let frequencyCounter2 = {};
+
+  for (let val of arr1) {
+    frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
+  }
+  for (let val of arr2) {
+    frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
+  }
+  for (let key in frequencyCounter1) {
+    if (!(key ** 2 in frequencyCounter2)) {
+      console.log(frequencyCounter1);
+      console.log(frequencyCounter2);
+      return false;
+    }
+    if (frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
+      return false;
+    }
+  }
+  console.log(frequencyCounter1);
+  console.log(frequencyCounter2);
+  return true;
+}
+
+console.log(same(arr1, arr2));
