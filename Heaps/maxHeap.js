3e56b15edc3844be6d43419cb7b363a84eb0287c
Karma Drukpa <karmadrukpa11@gmail.com>
2020-06-30 19:17:24 -0700

    Added maxHeap insert method

---
+++ Heaps/maxHeap.js
@@ -0,0 +1,37 @@
+class MaxBinaryHeap {
+	constructor() {
+		this.values = [];
+	}
+
+	insert(element) {
+		this.values.push(element);
+		this.bubbleUp();
+	}
+
+	bubbleUp() {
+		let index = this.values.length - 1;
+		const element = this.values[index];
+
+		while (index > 0) {
+			let parentIndex = Math.floor((index - 1) / 2);
+			let parent = this.values[parentIndex];
+
+			if (element <= parent) break;
+			this.values[parentIndex] = element;
+			this.values[index] = parent;
+			index = parentIndex;
+		}
+	}
+}
+
+let heap = new MaxBinaryHeap();
+
+heap.insert(55);
+heap.insert(39);
+heap.insert(33);
+heap.insert(18);
+heap.insert(27);
+heap.insert(12);
+heap.insert(200);
+
+console.log(heap);
