9e5810e3dfe1cd3620e44b68bddfde830fa38205
Karma Drukpa <karmadrukpa11@gmail.com>
2020-07-11 22:06:06 -0400

    Add heap swap method

--- Heaps/maxHeap.js
+++ Heaps/maxHeap.js
@@ -22,6 +22,51 @@ class MaxBinaryHeap {
 			index = parentIndex;
 		}
 	}
+
+	extractMax() {
+		// edge case come back
+		const max = this.values[0];
+		let end = this.values.pop();
+		this.values[0] = end;
+		// trickle down
+		this.sinkDown();
+		return max;
+	}
+
+	sinkDown() {
+		let idx = 0;
+		// find the children
+		const length = this.values.length;
+		const element = this.values[0];
+
+		while (true) {
+			let leftChildIdx = 2 * idx + 1;
+			let rightChildIdx = 2 * idx + 2;
+			let leftChild, rightChild;
+			let swap = null;
+
+			if (leftChild < length) {
+				leftChild = this.values[leftChildIdsx];
+				if (leftChild > element) {
+					swap = leftChildIdx;
+				}
+			}
+			if (rightChildIdx < length) {
+				rightChild = this.values[rightChildIdx];
+				if (
+					(swap === null && rightChild > element) ||
+					(swap !== null && rightChild > leftChild)
+				) {
+					swap = rightChildIdx;
+				}
+			}
+
+			if (swap === null) break;
+			this.values[idx] = this.values[swap];
+			this.values[swap] = element;
+			idx = swap;
+		}
+	}
 }
 
 let heap = new MaxBinaryHeap();
@@ -33,5 +78,6 @@ heap.insert(18);
 heap.insert(27);
 heap.insert(12);
 heap.insert(200);
+heap.extractMax();
 
 console.log(heap);
