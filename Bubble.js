8d0b3917c7b90777c4bc9df8f701e7958b909a39
Karma Drukpa <karmadrukpa11@gmail.com>
2020-07-27 16:17:20 -0400

    Fix buble sort solution and added other solutions

--- Bubble.js
+++ Bubble.js
@@ -1,13 +1,46 @@
 const bubbleSort = (arr) => {
-	for (let i = 0; i < arr.length; i++) {
-		for (let j = 0; j < arr.length; j++) {
+	for (let i = arr.length; i > 0; i--) {
+		for (let j = 0; j < i - 1; j++) {
 			if (arr[j] > arr[j + 1]) {
 				// swap
 				let temp = arr[j];
-				arr[j] = arr[i + j];
+				arr[j] = arr[j + 1];
 				arr[j + 1] = temp;
 			}
 		}
+	}
+
+	return arr;
+
+
+	// another method to swap ES 2015
+	const swap = (arr, idx1, idx2) =>
+		([arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]);
+
+	for (let i = arr.length; i > 0; i--) {
+		for (let j = 0; j < i - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				swap(arr, j, j + 1);
+			}
+		}
+	}
+	return arr;
+
+	// another solution with optimized version
+
+	let noSwap;
+
+	for (let i = arr.length; i > 0; i--) {
+		noSwap = true;
+		for (let j = 0; j < i - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				let temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+				noSwap = false;
+			}
+		}
+		if (noSwap) break;
 	}
 
 	return arr;
